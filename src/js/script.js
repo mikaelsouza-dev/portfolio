bd2e35f42b3f2afeef0fcea69c5b5ddaa2167a5b
Mikael Souza <99297849+mikaelsouza-dev@users.noreply.github.com>
2023-05-16 15:41:46 -0300

    animations added, desc to projects and skills

--- src/js/script.js
+++ src/js/script.js
@@ -36,4 +36,29 @@
     // TOOLTIP
     $('.social-links a').tooltip();
 
+    (function($){
+      new WOW().init();
+  })
+
+  let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progressup");
+    let progressValue = document.getElementById("progressup-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#a200ff ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  };
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
+
 })(jQuery);
